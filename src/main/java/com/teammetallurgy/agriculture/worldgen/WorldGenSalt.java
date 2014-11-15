d8c149cc117784fc9fdb24ab0834430ce9446d49
lclc98 <liam.c14785@gmail.com>
2014-11-16 07:45:36 +1000

    Removed final from local fields

--- src/main/java/com/teammetallurgy/agriculture/worldgen/WorldGenSalt.java
+++ src/main/java/com/teammetallurgy/agriculture/worldgen/WorldGenSalt.java
@@ -8,11 +8,8 @@ import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenerator;
-import net.minecraftforge.event.terraingen.TerrainGen;
 
 import java.util.Random;
-
-import static net.minecraftforge.event.terraingen.DecorateBiomeEvent.Decorate.EventType.CLAY;
 
 public class WorldGenSalt extends WorldGenerator implements IWorldGenerator
 {
@@ -27,11 +24,10 @@ public class WorldGenSalt extends WorldGenerator implements IWorldGenerator
     @Override
     public void generate(final Random randomGenerator, final int chunkX, final int chunkZ, final World world, final IChunkProvider chunkGenerator, final IChunkProvider chunkProvider)
     {
-        final boolean doGen = TerrainGen.decorate(world, randomGenerator, chunkX, chunkZ, CLAY);
-        for (int i = 0; doGen && i < saltPerChunk; ++i)
+        for (int i = 0; i < saltPerChunk; ++i)
         {
-            final int randX = chunkX * 16 + randomGenerator.nextInt(16) + 8;
-            final int randZ = chunkZ * 16 + randomGenerator.nextInt(16) + 8;
+            int randX = chunkX * 16 + randomGenerator.nextInt(16) + 8;
+            int randZ = chunkZ * 16 + randomGenerator.nextInt(16) + 8;
             this.generate(world, randomGenerator, randX, world.getTopSolidOrLiquidBlock(randX, randZ), randZ);
         }
     }
@@ -45,21 +41,21 @@ public class WorldGenSalt extends WorldGenerator implements IWorldGenerator
         }
         else
         {
-            final int l = par2Random.nextInt(2) + 2;
-            final byte b0 = 1;
+            int l = par2Random.nextInt(2) + 2;
+            byte b0 = 1;
 
             for (int i1 = par3 - l; i1 <= par3 + l; ++i1)
             {
                 for (int j1 = par5 - l; j1 <= par5 + l; ++j1)
                 {
-                    final int k1 = i1 - par3;
-                    final int l1 = j1 - par5;
+                    int k1 = i1 - par3;
+                    int l1 = j1 - par5;
 
                     if (k1 * k1 + l1 * l1 <= l * l)
                     {
                         for (int i2 = par4 - b0; i2 <= par4 + b0; ++i2)
                         {
-                            final Block j2 = par1World.getBlock(i1, i2, j1);
+                            Block j2 = par1World.getBlock(i1, i2, j1);
 
                             if (j2 == Blocks.dirt || j2 == Blocks.clay)
                             {
