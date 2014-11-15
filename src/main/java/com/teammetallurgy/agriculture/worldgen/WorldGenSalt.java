38067a23ff825eb77f9c41c020d4b7b296db1f46
lclc98 <liam.c14785@gmail.com>
2014-11-16 07:55:02 +1000

    Named parameters

--- src/main/java/com/teammetallurgy/agriculture/worldgen/WorldGenSalt.java
+++ src/main/java/com/teammetallurgy/agriculture/worldgen/WorldGenSalt.java
@@ -22,44 +22,44 @@ public class WorldGenSalt extends WorldGenerator implements IWorldGenerator
     }
 
     @Override
-    public void generate(final Random randomGenerator, final int chunkX, final int chunkZ, final World world, final IChunkProvider chunkGenerator, final IChunkProvider chunkProvider)
+    public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
     {
         for (int i = 0; i < saltPerChunk; ++i)
         {
-            int randX = chunkX * 16 + randomGenerator.nextInt(16) + 8;
-            int randZ = chunkZ * 16 + randomGenerator.nextInt(16) + 8;
-            this.generate(world, randomGenerator, randX, world.getTopSolidOrLiquidBlock(randX, randZ), randZ);
+            int randX = chunkX * 16 + random.nextInt(16) + 8;
+            int randZ = chunkZ * 16 + random.nextInt(16) + 8;
+            this.generate(world, random, randX, world.getTopSolidOrLiquidBlock(randX, randZ), randZ);
         }
     }
 
     @Override
-    public boolean generate(final World par1World, final Random par2Random, final int par3, final int par4, final int par5)
+    public boolean generate(World world, Random random, int x, int y, int z)
     {
-        if (par1World.getBlock(par3, par4, par5).getMaterial() != Material.water)
+        if (world.getBlock(x, y, z).getMaterial() != Material.water)
         {
             return false;
         }
         else
         {
-            int l = par2Random.nextInt(2) + 2;
+            int l = random.nextInt(2) + 2;
             byte b0 = 1;
 
-            for (int i1 = par3 - l; i1 <= par3 + l; ++i1)
+            for (int i1 = x - l; i1 <= x + l; ++i1)
             {
-                for (int j1 = par5 - l; j1 <= par5 + l; ++j1)
+                for (int j1 = z - l; j1 <= z + l; ++j1)
                 {
-                    int k1 = i1 - par3;
-                    int l1 = j1 - par5;
+                    int k1 = i1 - x;
+                    int l1 = j1 - z;
 
                     if (k1 * k1 + l1 * l1 <= l * l)
                     {
-                        for (int i2 = par4 - b0; i2 <= par4 + b0; ++i2)
+                        for (int i2 = y - b0; i2 <= y + b0; ++i2)
                         {
-                            Block j2 = par1World.getBlock(i1, i2, j1);
+                            Block j2 = world.getBlock(i1, i2, j1);
 
                             if (j2 == Blocks.dirt || j2 == Blocks.clay)
                             {
-                                par1World.setBlock(i1, i2, j1, saltBlock, 0, 2);
+                                world.setBlock(i1, i2, j1, saltBlock, 0, 2);
                             }
                         }
                     }
