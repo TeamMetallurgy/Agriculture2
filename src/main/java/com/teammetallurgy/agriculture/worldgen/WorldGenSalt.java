7550a608434d1e2ce58ed5f36eff027376f2e3d7
Glassmaker <JeanGlassmaker@mail.com>
2014-11-16 12:23:30 +0300

    Adjusts salt generation to avoid replacing clay, and more naming of the varibles

--- src/main/java/com/teammetallurgy/agriculture/worldgen/WorldGenSalt.java
+++ src/main/java/com/teammetallurgy/agriculture/worldgen/WorldGenSalt.java
@@ -1,20 +1,22 @@
 package com.teammetallurgy.agriculture.worldgen;
 
-import com.teammetallurgy.agriculture.BlockList;
-import cpw.mods.fml.common.IWorldGenerator;
+import java.util.Random;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
-import net.minecraft.world.gen.feature.WorldGenerator;
 
-import java.util.Random;
+import com.teammetallurgy.agriculture.BlockList;
 
-public class WorldGenSalt extends WorldGenerator implements IWorldGenerator
+import cpw.mods.fml.common.IWorldGenerator;
+
+public class WorldGenSalt implements IWorldGenerator
 {
     private int saltPerChunk = 1;
     private Block saltBlock;
+    private static int saltSeed = "saltBlock".hashCode();
 
     public WorldGenSalt()
     {
@@ -24,42 +26,44 @@ public class WorldGenSalt extends WorldGenerator implements IWorldGenerator
     @Override
     public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator, IChunkProvider chunkProvider)
     {
-        for (int i = 0; i < saltPerChunk; ++i)
+
+        Random chunkRandom = new Random(random.nextLong() + saltSeed);
+
+        for (int i = 0; i < saltPerChunk; i++)
         {
-            int randX = chunkX * 16 + random.nextInt(16) + 8;
-            int randZ = chunkZ * 16 + random.nextInt(16) + 8;
-            this.generate(world, random, randX, world.getTopSolidOrLiquidBlock(randX, randZ), randZ);
+            int randX = chunkX * 16 + chunkRandom.nextInt(16) + 8;
+            int randZ = chunkZ * 16 + chunkRandom.nextInt(16) + 8;
+            this.generate(world, chunkRandom, randX, world.getTopSolidOrLiquidBlock(randX, randZ), randZ);
         }
     }
 
-    @Override
-    public boolean generate(World world, Random random, int x, int y, int z)
+    public boolean generate(World world, Random random, int centerX, int centerY, int centerZ)
     {
-        if (world.getBlock(x, y, z).getMaterial() != Material.water)
+        if (world.getBlock(centerX, centerY, centerZ).getMaterial() != Material.water)
         {
             return false;
         }
         else
         {
-            int l = random.nextInt(2) + 2;
-            byte b0 = 1;
+            int raidus = random.nextInt(2) + 2;
+            byte heightRaidus = 1;
 
-            for (int i1 = x - l; i1 <= x + l; ++i1)
+            for (int posX = centerX - raidus; posX <= centerX + raidus; posX++)
             {
-                for (int j1 = z - l; j1 <= z + l; ++j1)
+                for (int posZ = centerZ - raidus; posZ <= centerZ + raidus; posZ++)
                 {
-                    int k1 = i1 - x;
-                    int l1 = j1 - z;
+                    int deltaX = posX - centerX;
+                    int deltaZ = posZ - centerZ;
 
-                    if (k1 * k1 + l1 * l1 <= l * l)
+                    if (deltaX * deltaX + deltaZ * deltaZ <= raidus * raidus)
                     {
-                        for (int i2 = y - b0; i2 <= y + b0; ++i2)
+                        for (int posY = centerY - heightRaidus; posY <= centerY + heightRaidus; posY++)
                         {
-                            Block j2 = world.getBlock(i1, i2, j1);
+                            Block block = world.getBlock(posX, posY, posZ);
 
-                            if (j2 == Blocks.dirt || j2 == Blocks.clay)
+                            if (block == Blocks.dirt || block == saltBlock)
                             {
-                                world.setBlock(i1, i2, j1, saltBlock, 0, 2);
+                                world.setBlock(posX, posY, posZ, saltBlock, 0, 2);
                             }
                         }
                     }
