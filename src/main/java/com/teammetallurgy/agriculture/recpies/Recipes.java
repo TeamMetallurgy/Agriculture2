00fabaf4a029053f6a6452edda0c296ee58fddef
Glassmaker <JeanGlassmaker@mail.com>
2015-05-30 07:36:05 +0300

    Fixes creating recipes for food processor

--- src/main/java/com/teammetallurgy/agriculture/recpies/Recipes.java
+++ src/main/java/com/teammetallurgy/agriculture/recpies/Recipes.java
@@ -2,6 +2,8 @@ package com.teammetallurgy.agriculture.recpies;
 
 import java.util.ArrayList;
 
+import com.teammetallurgy.agriculture.Agriculture;
+
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -34,8 +36,21 @@ public class Recipes
             if (ingredient2List != null && ingredient2List.size() > 0 && ingredient2List.get(0) != null) ingredient2 = ingredient2List.get(0).copy();
             if (outputList != null && outputList.size() > 0 && outputList.get(0) != null) output = outputList.get(0).copy();
 
-            if (ingredient1 == null || output == null) continue;
-            if (ingredient2 == null) processorRecipes.add(new RecipeProcessor(ingredient1, output));
+            if (ingredient1 == null || (ingredient2 == null && !tempProcessorRecipe[1].equalsIgnoreCase("")) || output == null)
+            {
+                String recipe = tempProcessorRecipe[0];
+                if (!tempProcessorRecipe[1].equalsIgnoreCase(""))
+                {
+                    recipe += " + " + tempProcessorRecipe[1];
+                }
+                recipe +=  " --> " + tempProcessorRecipe[2];
+                
+                Agriculture.logger.error("processing recipes: ingredients or output not found for recipe: " + recipe);
+                
+                continue;
+            }
+            
+            if (tempProcessorRecipe[1].equalsIgnoreCase(""))  processorRecipes.add(new RecipeProcessor(ingredient1, output));
             else processorRecipes.add(new RecipeProcessor(ingredient1, ingredient2, output));
 
         }
