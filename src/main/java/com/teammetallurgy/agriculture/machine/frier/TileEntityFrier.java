af8540e282eabe0bfcc574917182a077838a4ca1
Glassmaker <JeanGlassmaker@mail.com>
2015-11-07 13:26:57 +0300

    Adds fluid handling for Frier

--- src/main/java/com/teammetallurgy/agriculture/machine/frier/TileEntityFrier.java
+++ src/main/java/com/teammetallurgy/agriculture/machine/frier/TileEntityFrier.java
@@ -2,14 +2,24 @@ package com.teammetallurgy.agriculture.machine.frier;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntityFurnace;
+import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTank;
+import net.minecraftforge.fluids.FluidTankInfo;
+import net.minecraftforge.fluids.IFluidHandler;
 
 import com.teammetallurgy.agriculture.machine.TileEntityBaseMachine;
+import com.teammetallurgy.agriculture.utils.AgricultureDirection;
 
-public class TileEntityFrier extends TileEntityBaseMachine
+public class TileEntityFrier extends TileEntityBaseMachine implements IFluidHandler
 {
     private static final int[] INPUT_SLOTS = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     private static final int FUEL_SLOT = 0;
     private static final int[] OUTPUT_SLOTS = new int[] { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    private FluidTank tank = new FluidTank(8000);
+    private static int MAX_FLUID_TRANSFER = 1000;
 
     public TileEntityFrier()
     {
@@ -60,5 +70,94 @@ public class TileEntityFrier extends TileEntityBaseMachine
     public boolean canExtractItem(int slotId, ItemStack itemStack, int side)
     {
         return side == 0 && slotId >= 2 && slotId <= 10;
+    }
+
+    // Implementing IFluidHandler
+    public int fill(ForgeDirection from, FluidStack resource, boolean doFill)
+    {
+        if (resource == null) { return 0; }
+
+        FluidStack fillingStack = resource.copy();
+        if (fillingStack.amount > MAX_FLUID_TRANSFER)
+        {
+            fillingStack.amount = MAX_FLUID_TRANSFER;
+        }
+
+        int amount = tank.fill(resource, doFill);
+        if (doFill)
+        {
+            markDirty();
+        }
+        return amount;
+    }
+
+    @Override
+    public FluidStack drain(ForgeDirection from, FluidStack resource, boolean doDrain)
+    {
+        if (resource == null || !resource.isFluidEqual(tank.getFluid())) { return null; }
+
+        int withdrawAmount = resource.amount;
+        if (withdrawAmount > MAX_FLUID_TRANSFER)
+        {
+            withdrawAmount = MAX_FLUID_TRANSFER;
+        }
+
+        if (doDrain)
+        {
+            markDirty();
+        }
+
+        return tank.drain(withdrawAmount, doDrain);
+    }
+
+    @Override
+    public FluidStack drain(ForgeDirection from, int maxDrain, boolean doDrain)
+    {
+        int withdrawAmount = maxDrain;
+        if (withdrawAmount > MAX_FLUID_TRANSFER)
+        {
+            withdrawAmount = MAX_FLUID_TRANSFER;
+        }
+
+        if (doDrain)
+        {
+            markDirty();
+        }
+
+        return tank.drain(withdrawAmount, doDrain);
+    }
+
+    @Override
+    public boolean canFill(ForgeDirection from, Fluid fluid)
+    {
+        AgricultureDirection source = AgricultureDirection.convertFromForgeDirection(from);
+        AgricultureDirection facingDirection = AgricultureDirection.getDirectionFromIndex(facing);
+
+        if (facingDirection.getLeft() == source && (tank.getFluid() == null || tank.getFluid().getFluid() == fluid)) { return true; }
+        return false;
+    }
+
+    @Override
+    public boolean canDrain(ForgeDirection from, Fluid fluid)
+    {
+        AgricultureDirection source = AgricultureDirection.convertFromForgeDirection(from);
+        AgricultureDirection facingDirection = AgricultureDirection.getDirectionFromIndex(facing);
+
+        if (facingDirection.getRight() == source) { return true; }
+        return false;
+    }
+
+    @Override
+    public FluidTankInfo[] getTankInfo(ForgeDirection from)
+    {
+        AgricultureDirection source = AgricultureDirection.convertFromForgeDirection(from);
+        AgricultureDirection facingDirection = AgricultureDirection.getDirectionFromIndex(facing);
+
+        if (facingDirection.getRight() == source)
+        {
+            return new FluidTankInfo[] { tank.getInfo() };
+        }
+        else if (facingDirection.getRight() == source) { return new FluidTankInfo[] { tank.getInfo() }; }
+        return null;
     }
 }
