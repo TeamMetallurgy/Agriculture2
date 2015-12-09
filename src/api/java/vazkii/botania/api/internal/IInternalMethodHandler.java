0ae74f7b7b661fce03c4867fcbab54e0d535f9e1
Glassmaker <JeanGlassmaker@mail.com>
2015-12-09 16:16:12 +0300

    Updates Botania's API

--- src/api/java/vazkii/botania/api/internal/IInternalMethodHandler.java
+++ src/api/java/vazkii/botania/api/internal/IInternalMethodHandler.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
@@ -93,6 +94,8 @@ public interface IInternalMethodHandler {
 	public IInventory getBaublesInventory(EntityPlayer player);
 
 	public void breakOnAllCursors(EntityPlayer player, Item item, ItemStack stack, int x, int y, int z, int side);
+
+	public boolean hasSolegnoliaAround(Entity e);
 
 	@SideOnly(Side.CLIENT)
 	public void drawSimpleManaHUD(int color, int mana, int maxMana, String name, ScaledResolution res);
